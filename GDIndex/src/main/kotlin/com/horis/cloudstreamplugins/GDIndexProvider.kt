40dd8ed52dc45ba66265a1e50e64b955258ad196
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-08-31 12:13:15 +0530

    try

--- GDIndex/src/main/kotlin/com/horis/cloudstreamplugins/GDIndexProvider.kt
+++ GDIndex/src/main/kotlin/com/horis/cloudstreamplugins/GDIndexProvider.kt
@@ -120,7 +120,7 @@ class GDIndexProvider : MainAPI() {
                 ExtractorLinkType.VIDEO,
             ) {
                 this.referer = "$mainUrl/"
-                this.headers = mapOf("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                this.headers = mapOf("User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
             }
         )
         return true
