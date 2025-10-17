44e741cec11a07e2a899e79030ad22a26388c97e
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-10-17 10:25:35 +0530

    try

--- CineStream/src/main/kotlin/com/megix/CineStreamUtils.kt
+++ CineStream/src/main/kotlin/com/megix/CineStreamUtils.kt
@@ -484,6 +484,25 @@ fun fixUrl(url: String, domain: String): String {
     }
 }
 
+//Xprime
+suspend fun multiDecrypt(text : String, source: String) : String {
+    val headers = mapOf(
+        "Content-Type" to "application/json",
+        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept" to "application/json",
+        "Accept-Language" to "en-US,en;q=0.9'",
+    )
+    val json = app.post(
+        "https://enc-dec.app/api/$source",
+        headers = headers,
+        data = mapOf(
+            "text" to text
+        )
+    ).text
+
+    return json
+}
+
 suspend fun bypassHrefli(url: String): String? {
     fun Document.getFormUrl(): String {
         return this.select("form#landing").attr("action")
@@ -1406,7 +1425,7 @@ fun parseServers(jsonString: String): List<TripleOneMoviesServer> {
 
  fun customEncode(input: ByteArray): String {
     val sourceChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-    val targetChars = "E2kdP-yKbuMQFIV917Stvf8nsU3NicYG6OgmCxzAe5wHLZR0rTa_DhWolBjJ4qXp"
+    val targetChars = "qKSV9YJhF-WvABcOTH6P1ewt8NrfkREg3LZjU2b_GnmyaDI5piX7xz0CoMuQdl4s"
 
     val translationMap = sourceChars.zip(targetChars).toMap()
     val encoded = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
