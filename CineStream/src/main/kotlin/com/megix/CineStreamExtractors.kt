f5a5073d2bcca3b9ac356adf656dc9151e73a34c
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-11-08 16:57:04 +0530

    try

--- CineStream/src/main/kotlin/com/megix/CineStreamExtractors.kt
+++ CineStream/src/main/kotlin/com/megix/CineStreamExtractors.kt
@@ -1977,15 +1977,6 @@ object CineStreamExtractors : CineStreamProvider() {
             "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
         )
         val res = app.get(url, headers = headers, timeout = 200L).parsedSafe<TorrentioResponse>()
-
-        callback.invoke(
-            newExtractorLink(
-                "Comet",
-                "Comet",
-                res.toString()
-            )
-        )
-
         val resp = app.get(TRACKER_LIST_URL).text
         val sourceTrackers = resp
             .split("\n")
@@ -1994,18 +1985,9 @@ object CineStreamExtractors : CineStreamProvider() {
 
         res?.streams?.forEach { stream ->
             val title = stream.title ?: stream.name ?: ""
-
-            callback.invoke(
-                newExtractorLink(
-                    "title",
-                    "title",
-                    title
-                )
-            )
-
             val regex = Regex("""\uD83D\uDC64\s*(\d+)""")
             val match = regex.find(title)
-            val seeders = match?.groupValues?.get(1)?.toInt() ?: 0
+            val seeders = match?.groupValues?.get(1)?.toInt() ?: 21
             if (seeders < 20) return@forEach
             val magnet = "magnet:?xt=urn:btih:${stream.infoHash}&dn=${stream.infoHash}$sourceTrackers&index=${stream.fileIdx}"
             callback.invoke(
