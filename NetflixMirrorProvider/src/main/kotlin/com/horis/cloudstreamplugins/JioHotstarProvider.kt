cd92f799c55460fd81e7d28e62df27805b4b498b
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2026-02-15 11:51:14 +0530

    try

--- NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/JioHotstarProvider.kt
+++ NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/JioHotstarProvider.kt
@@ -27,7 +27,7 @@ class JioHotstarProvider : MainAPI() {
     )
     override var lang = "en"
 
-    override var mainUrl = "https://net22.cc"
+    override var mainUrl = "https://net52.cc"
     private var newUrl = "https://net52.cc"
     override var name = "JioHotstar"
 
